2dcb1e14f4ec0d90b1dad3ca03ebba7fa5a3d3ff
Alex Samuel <alex@alexsamuel.net>
2018-04-16 23:37:55 -0400

    Improve CLI.

--- python/absence/cli.py
+++ python/absence/cli.py
@@ -10,18 +10,36 @@ from   absence.model import Absence
 #-------------------------------------------------------------------------------
 
 def cmd_add(client, args):
-    start = parse_date(args.start)
-    end = parse_date(args.end)
-    end = start if end is None else end
-    dates = slice(start, end + DAY)
-    absence = Absence(args.name, dates, args.code, args.notes)
-    absence = client.insert(absence)
-    print(absence.id)
+    start   = parse_date(args.start)
+    end     = parse_date(args.end)
+    end     = start if end is None else end
+    dates   = slice(start, end + DAY)
+    abc     = Absence(args.name, dates, args.code, args.notes)
+    abc     = client.insert(abc)
+    print(abc.id)
 
 
 def cmd_show(client, args):
-    for rec in client.search():
-        print(rec)
+    abcs = list(client.search())
+
+    start   = datetime.date.today()
+    days    = 7
+    for d in range(days):
+        date = start + d * DAY
+        date_abcs = [ a for a in abcs if a.dates.start <= date < a.dates.stop ]
+        date_abcs = sorted(date_abcs, key=lambda a: a.name)
+        if len(date_abcs) == 0:
+            date_recs = ["(none)"]
+        else:
+            date_recs = [
+                "{:12s} {:12s}   {}".format(
+                    a.name, a.code, "" if a.notes is None else a.notes)
+                for a in date_abcs
+            ]
+
+        for i, rec in enumerate(date_recs):
+            print("{:10s}  {}".format(str(date) if i == 0 else "", rec))
+        print()
 
 
 def cmd_default(client, args):
