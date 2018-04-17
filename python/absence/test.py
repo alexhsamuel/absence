be29c1cd2ce442114468d11b36ba61a7e36878df
Alex Samuel <alex@alexsamuel.net>
2018-04-16 23:20:04 -0400

    Random population.

--- python/absence/test.py
+++ python/absence/test.py
@@ -1,23 +1,44 @@
 from   datetime import date
+import random
+import string
 
-import absence.db
+from   absence.dates import DAY
 from   absence.model import Absence
 
 #-------------------------------------------------------------------------------
 
-def create_db(path):
-    db = absence.db.SqliteDB.create(path)
-    db.insert(Absence(
-        "asamuel", 
-        slice(date(2018, 2, 1), date(2018, 3, 1)),
-        "vacation", 
-        "In Kamchatka.",
-    ))
-    db.insert(Absence(
-        "asamuel", 
-        slice(date(2018, 4, 1), date(2018, 5, 11)),
-        "vacation", 
-        "In Antarctica.",
-    ))
+NAMES = (
+    "asamuel", "rblankenhorn", "ggundersen", "awhitney", "dlovell", "jgoodman", 
+    "jpowers", "schuah", "fjanoos", "awestbrook", "claventhal", "ggrigorov",
+)
+
+CODES = ("vacation", "remote", "medical", "personal", )
+
+def word():
+    return "".join( 
+        random.choice(string.ascii_lowercase) 
+        for _ in range(random.randint(2, 10))
+    )
+
+
+def populate(db):
+    for i in range(random.randint(10, 30)):
+        start = date(2018, 1, 1) + random.randint(0, 365) * DAY
+        if random.random() < 0.5:
+            dates = slice(start, start + DAY)
+        else:
+            dates = slice(start, start + random.randint(1, 14) * DAY)
+        if random.random() < 0.5:
+            notes = None
+        else:
+            notes = " ".join( word() for _ in range(random.randint(2, 8)) )
+            notes = notes.capitalize() + "."
+        
+        db.insert(Absence(
+            random.choice(NAMES),
+            dates,
+            random.choice(CODES),
+            notes,
+        ))
 
 
